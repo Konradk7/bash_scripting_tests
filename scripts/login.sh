c898829a6a461c3c64680b4b89cd41b009b2ca61
Konradk7 <konrad.kdro7@gmail.com>
2025-06-04 17:17:37 +0200

    add login script

---
+++ scripts/login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter Your skills:"
+read SKILL
+
+echo "Your $SKILL is super nice!"
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+
+echo 'login successfull: Welcome $USR.'
